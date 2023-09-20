4a61ff54c8d38c40cf47bce57b81950c1da2af66
Dolapo Soares <dsoares@econnectnp.com>
2023-09-20 22:03:08 +0100

    styling the data generated

--- aa-llc/app/index.js
+++ aa-llc/app/index.js
@@ -1,48 +1,60 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { useEffect, useState } from 'react';
-
+import React from "react";
+import { useEffect, useState } from "react";
 
 const Index = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await fetch('https://randomuser.me/api/'); // Replace with your API URL
-          const jsonData = await response.json();
-          const results = jsonData.results;
-          
-          setData(results);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          setLoading(false);
-        }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://randomuser.me/api/"); // Replace with your API URL
+        const jsonData = await response.json();
+        const results = jsonData.results;
+
+        setData(results);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
       }
-  
-      fetchData();
-    }, []);
+    }
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1> Welcome to My blog gallery ssg</h1>
-        {data.map((item, index) =>(
-        <div key={index}>
-            <div>
-            <img src={item.picture.large} />
-            </div>
-            <div>
-                <p>Name : {item.name.title} {item.name.first} {item.name.last}</p>
-                <p>Gender : {item.gender} </p>
-            </div>
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">
+        Welcome to the details of random people
+      </h1>
+      {data.map((item, index) => (
+        <div className="flex mb-4" key={index}>
+          <div className="w-1/2 p-4">
+            <img
+              src={item.picture.large}
+              alt={`${item.name.first} ${item.name.last}`}
+            />
+          </div>
+          <div className="w-1/2 p-4">
+            <p className="text-lg font-semibold">
+              {item.name.title} {item.name.first} {item.name.last}
+            </p>
+            <p>Gender: {item.gender}</p>
+            <p>Email: {item.email}</p>
+            <p>Age: {item.dob.age}</p>
+            <p>Phone No: {item.cell}</p>
+            <p>
+              Address: {item.location.street.number},{" "}
+              {item.location.street.name}, {item.location.city},{" "}
+              {item.location.state}, {item.location.country}.
+            </p>
+          </div>
         </div>
-        
-    ))}
+      ))}
     </div>
-  )
-}
-export default Index
+  );
+};
+export default Index;
--- aa-llc/app/page.js
+++ aa-llc/app/page.js
@@ -4,7 +4,7 @@ import { getServerSideProps } from './index'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <Index />
       </div>
